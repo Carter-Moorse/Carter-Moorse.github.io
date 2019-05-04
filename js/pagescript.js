9871ba2e47cf68540c2121d4661f8368b6ded16d
Carter Moorse <cartermoorse@gmail.com>
2019-05-04 21:36:41 +0100

    Added slideshow

--- js/pagescript.js
+++ js/pagescript.js
@@ -60,60 +60,165 @@ btn.addEventListener('click', (e)=>{
     e.currentTarget.classList.toggle('close');
     leftContainer.classList.toggle('active');
     active = active ? false : true;
-    // console.log(active);
     if (active) window.addEventListener('mousedown', closeMenu);
     else window.removeEventListener('mousedown', closeMenu);
 });
 
 // Slideshow
 
-// let slideshows = document.getElementsByClassName('slideshow');
-// let activeSlideShows = [];
+let slideshows = document.getElementsByClassName('slideshow');
+let activeSlideShows = [];
 
-// function setupSlideshow(show) {
-//     let context = this;
+function setupSlideshow(show) {
+    let context = this;
 
-//     this.currentSlide = show.querySelector('img:first-child');
-//     this.nextSlide = this.currentSlide.nextElementSibling;
-//     this.prevSlide = null;
-//     this.setSlides = function() {
-//         this.currentSlide.style.marginLeft = null;
-//         if (this.nextSlide) {
-//             this.btnRight.classList.remove('hide');
-//             this.nextSlide.style.marginLeft = '100%';
-//         }
-//         else this.btnRight.classList.add('hide');
-//         if (this.prevSlide) {
-//             this.btnLeft.classList.remove('hide');
-//             this.prevSlide.style.marginLeft = '-100%';
-//         }
-//         else this.btnLeft.classList.add('hide');
-//     }
-//     this.slide = function(el) {
-//         if (el != null) {
-//             this.currentSlide = el;
-//             this.nextSlide = el.nextElementSibling != null && el.nextElementSibling.nodeName == 'IMG' ? el.nextElementSibling : null;
-//             this.prevSlide = el.previousElementSibling != null && el.previousElementSibling.nodeName == 'IMG' ? el.previousElementSibling : null;
-//             this.setSlides();
-//         }
-//     }
+    // Slideshow variables
+    this.slides = show.querySelectorAll('img');
+    this.slideIndex = 0;
+    this.currentSlide = show.querySelector('img:first-child');
+    this.nextSlide = this.currentSlide.nextElementSibling;
+    this.prevSlide = null;
 
-//     // Setup
-//     if (show.childNodes.length > 3) {
-//         show.insertAdjacentHTML('afterBegin', '<div class="right">></div><div class="left"><</div>');
+    // Paging wrapper variable
+    this.pagingWrapper = null;
 
-//         //Buttons
-//         this.btnRight = show.querySelector('.right');
-//         this.btnLeft = show.querySelector('.left');
+    // Transition styling variable
+    this.transition = '0.8s all cubic-bezier(0.72, 0, 0.24, 1)';
 
-//         this.btnLeft.classList.add('hide');
+    // Change the slide to the new index    
+    this.changeSlides = function() {
+        this.currentSlide = this.slides[this.slideIndex];
+        this.nextSlide = this.currentSlide.nextElementSibling != null && this.currentSlide.nextElementSibling.nodeName == 'IMG' ? this.currentSlide.nextElementSibling : null;
+        this.prevSlide = this.currentSlide.previousElementSibling != null && this.currentSlide.previousElementSibling.nodeName == 'IMG' ? this.currentSlide.previousElementSibling : null;
+        
+        // Next slide is available
+        if (this.nextSlide) {
+            this.btnRight.addEventListener('click', this.goNextSlide);
+            this.btnRight.classList.remove('hide');
+        }
+        // ... isn't available
+        else {
+            this.btnRight.classList.add('hide');
+            this.btnRight.removeEventListener('click', this.goNextSlide);
+        }
+        // Previous slide is available
+        if (this.prevSlide) {
+            this.btnLeft.addEventListener('click', this.goPrevSlide);
+            this.btnLeft.classList.remove('hide');
+        }
+        // ... isn't available
+        else {
+            this.btnLeft.classList.add('hide');
+            this.btnLeft.removeEventListener('click', this.goPrevSlide);
+        }
 
-//         this.btnRight.addEventListener('click', this.btnClickRight = ()=>   {context.slide(context.nextSlide);});
-//         this.btnLeft.addEventListener('click', this.btnClickLeft = ()=>     {context.slide(context.prevSlide);});
-//     }
-// }
+        // Move the current slide in view
+        this.currentSlide.style.transition = this.transition;
+        window.requestAnimationFrame(() => {
+            this.currentSlide.style.marginLeft = null;
+            this.currentSlide.style.position = 'relative';
+        });
 
-// for (i = 0; i < slideshows.length; i++) {
-//     let slideshow = slideshows[i];
-//     activeSlideShows.push(new setupSlideshow(slideshow));
-// }
+        // Move the other slides out of view
+        this.slides.forEach((el, x) => {
+            if (x != this.slideIndex) {
+                // The next slides
+                if (x > this.slideIndex) {
+                    el.style.transition = this.transition;
+                    window.requestAnimationFrame(() => { 
+                        el.style.marginLeft = '100%';
+                        el.style.position = 'absolute';
+                    });
+                }
+                // The previous slides
+                else {
+                    el.style.transition = this.transition;
+                    window.requestAnimationFrame(() => { 
+                        el.style.marginLeft = '-100%';
+                        el.style.position = 'absolute';
+                    });
+                }
+            }
+        });
+
+        context.pagingWrapper.querySelector('.paging-pages.select').style.left = 15 * context.slideIndex + 'px';
+    }
+
+    // Goto the next available slide
+    this.goNextSlide = function(e) {
+        ++context.slideIndex;
+        context.changeSlides();
+    }
+
+    // Goto the previous available slide
+    this.goPrevSlide = function(e) {
+        --context.slideIndex;
+        context.changeSlides();
+    }
+
+    // Goto index of slide
+    this.goSlide = function(slide) {
+        if (slide > this.slides.length) {
+            console.log('Slide number too large!', this);
+        }
+        else {
+            context.slideIndex = slide;
+            context.changeSlides();
+        }
+    }
+
+    // Setup
+    this.activate = function() {
+        if (this.slides.length > 2) {
+            // Creating slideshow buttons
+            show.insertAdjacentHTML('afterBegin', '<div class="right">></div><div class="left"><</div>');
+    
+            // Saving slideshow buttons in variables
+            this.btnRight = show.querySelector('.right');
+            this.btnLeft = show.querySelector('.left');
+    
+            // Hiding the previous button from start
+            this.btnLeft.classList.add('hide');
+    
+            // Creating paging wrapper
+            show.insertAdjacentHTML('afterBegin', '<div class="paging"></div>');
+    
+            // Saving paging wrapper
+            this.pagingWrapper = show.querySelector('.paging');
+    
+            this.slides.forEach((el, i) => {
+    
+                // Setting up the next slides in position
+                if (el != context.currentSlide) {
+                    el.style.marginLeft = '100%';
+                    el.style.position = 'absolute';
+                }
+    
+                // Creating slide paging inside the paging wrapper
+                this.pagingWrapper.insertAdjacentHTML('beforeend', `<div class='paging-pages' id='${i}'></div>`);
+            });
+    
+            // Creating the activity dot that indicates what slide is showing
+            this.pagingWrapper.insertAdjacentHTML('beforeend', `<div class='paging-pages select' style='transition: ${context.transition};'></div>`)
+    
+            // Adding click events to the left/right buttons
+            this.btnRight.addEventListener('click', this.goNextSlide);
+            this.btnLeft.addEventListener('click', this.goPrevSlide);
+    
+            // Adding click events to the paging buttons
+            this.pagingWrapper.querySelectorAll('.paging-pages:not(.select)').forEach((el, i) => {
+                el.addEventListener('click', (e) => { context.goSlide(i); });
+            });
+        }
+        else {
+            console.log('Not enough "<img>" elements to activate slideshow (Minimum 2)')
+        }
+    }
+}
+
+// Loop to create the slideshows on the page
+for (i = 0; i < slideshows.length; i++) {
+    let slideshow = slideshows[i];
+    activeSlideShows.push(new setupSlideshow(slideshow));
+    activeSlideShows[i].activate();
+}
